3d13a79b8745f7aee7a885beeeb7cd2a7d7b1a68
BorisBil <95983063+BorisBil@users.noreply.github.com>
2022-11-20 15:05:15 -0500

    Generator Changes
    Changed the method of card generation by using JavaScript to create an array instead

---
+++ app.js
@@ -0,0 +1,43 @@
+const section = document.querySelector("section");
+const playerLives = document.querySelector("span");
+const Lives = 6;
+
+playerLives.textContent = Lives;
+
+const getImages = () => [
+    {imgSrc: "./img/budgie.jpg", name: "budgie"},
+    {imgSrc: "./img/crow.jpg", name: "crow"},
+    {imgSrc: "./img/cockatiel.jpg", name: "cockatiel"},
+    {imgSrc: "./img/cockatiel-header.jpg", name: "cockatiel-header"},
+    {imgSrc: "./img/cockatoo.jpg", name: "cockatoo"},
+    {imgSrc: "./img/macaw.jpg", name: "macaw"},
+    {imgSrc: "./img/budgie.jpg", name: "budgie"},
+    {imgSrc: "./img/crow.jpg", name: "crow"},
+    {imgSrc: "./img/cockatiel.jpg", name: "cockatiel"},
+    {imgSrc: "./img/cockatiel-header.jpg", name: "cockatiel-header"},
+    {imgSrc: "./img/cockatoo.jpg", name: "cockatoo"},
+    {imgSrc: "./img/macaw.jpg", name: "macaw"},
+];
+
+const shuffle = () => {
+    const cardData = getData();
+    cardData.sort(() => Math.random() - 0.5);
+    return cardData;
+};
+
+const generator = () => {
+    const cardData = shuffle();
+    cardData.forEach(item => {
+        const card = document.createElement("div");
+        const face = document.createElement("img");
+        const back = document.createElement("div");
+        card.classList = "card";
+        face.classList = "face";
+        back.classList = "back";
+        section.appendChild(card);
+        card.appendChild(face);
+        card.appendChild(back);
+    });
+};
+
+generator();
